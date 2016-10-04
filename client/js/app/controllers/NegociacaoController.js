d06cdb1cf4eb95fe6d18ed41c9f923f0ed15854c
Humberto Pinheiro <humbhenri@gmail.com>
2016-10-04 20:54:22 -0300

    adiciona model e controller

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,11 @@
+class NegociacaoController {
+
+    adiciona(event) {
+        event.preventDefault();
+        let $ = document.querySelector.bind(document);
+        let inputQuantidade = $('#quantidade');
+        let inputData = $('#data');
+        let inputValor = $('#valor');
+
+    }
+}
