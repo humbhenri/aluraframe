74b3c09935d51dd7cfbf7e553d934173ac9c0243
Humberto Pinheiro <humbhenri@gmail.com>
2016-10-05 19:43:06 -0300

    instantiate model in controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,11 +1,22 @@
 class NegociacaoController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this.inputQuantidade = $('#quantidade');
+        this.inputData = $('#data');
+        this.inputValor = $('#valor');
+    }
+
     adiciona(event) {
         event.preventDefault();
-        let $ = document.querySelector.bind(document);
-        let inputQuantidade = $('#quantidade');
-        let inputData = $('#data');
-        let inputValor = $('#valor');
-
+        let negociacao = new Negociacao(
+            new Date(...this.inputData.value
+                .split('-')
+                .map((item, index) => item - index % 2)),
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
+        console.log(negociacao);
+        // adiciona negociação em lista
     }
 }
