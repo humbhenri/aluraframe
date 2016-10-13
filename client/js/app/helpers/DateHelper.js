c7a8739e4838994df1249ae8333b628c2080e15d
Humberto Pinheiro <humbhenri@gmail.com>
2016-10-13 18:48:45 -0300

    view

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,15 +1,20 @@
 class DateHelper {
-
+    
     constructor() {
-        throw new Error("Cannot instantiate this class");
+        
+        throw new Error('Esta classe não pode ser instanciada');
     }
-
-    static textToDate(text) {
-        return new Date(...text.split('-')
-            .map((item, index) => item - index % 2));
+    
+    static dataParaTexto(data) {
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
-
-    static dateToText(date) {
-        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+    
+    static textoParaData(texto) {
+        
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+             
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
+    
 }
